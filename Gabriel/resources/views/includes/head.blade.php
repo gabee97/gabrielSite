fec97d74d52859d52b3cfb1f724416defc983f99
Gabriel Oliveira <gaabriel123@hotmail.com>
2024-08-25 11:03:18 -0300

    Criado meta tags

--- Gabriel/resources/views/includes/head.blade.php
+++ Gabriel/resources/views/includes/head.blade.php
@@ -1,9 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@lang('messages.gabriel-complete-name')</title>
+    <title>Gabriel Oliveira - Supervisor de TI | Experiência em PHP e Laravel</title>
+
+    <!-- Meta tags para SEO -->
+    <meta name="description" content="Gabriel Oliveira é Supervisor de TI com mais de 7 anos de experiência, especializado em PHP e Laravel. Explore meu site para conhecer mais sobre minhas habilidades e experiências.">
+    <meta name="keywords" content="Gabriel Oliveira, Supervisor de TI, PHP, Laravel, Programação, Experiência em TI">
+    <meta name="author" content="Gabriel Oliveira">
+
+    <!-- Meta tags para Open Graph (compartilhamento em redes sociais) -->
+    <meta property="og:title" content="Gabriel Oliveira - Supervisor de TI | Experiência em PHP e Laravel">
+    <meta property="og:description" content="Conheça mais sobre Gabriel Oliveira, um Supervisor de TI com mais de 7 anos de experiência em PHP e Laravel.">
+    <meta property="og:image" content="{{ asset('storage/5.jpeg') }}">
+    <meta property="og:url" content="https://gbr.dev.br">
+    <meta property="og:type" content="website">
+
+    <!-- Meta tags para Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="Gabriel Oliveira - Supervisor de TI | Experiência em PHP e Laravel">
+    <meta name="twitter:description" content="Conheça mais sobre Gabriel Oliveira, um Supervisor de TI com mais de 7 anos de experiência em PHP e Laravel.">
+    <meta name="twitter:image" content="{{ asset('storage/5.jpeg') }}">
+
     @include('includes/cdn')
 </head>
-<body>
